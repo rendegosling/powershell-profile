779f1c1a9797195b56d983de204776e6a948eeb2
Ren <renlesterdg@gmail.com>
2024-07-30 12:57:12 +1000

    auto reloading

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,8 +1,10 @@
 # Profile version (Semantic Versioning)
-$profileVersion = "1.0.4"
+$profileVersion = "1.0.6"
 
 # Constants
-Set-Variable -Name GITHUB_PROFILE_URL -Value "https://raw.githubusercontent.com/rendegosling/powershell-profile/main/Microsoft.Powershell_profile.ps1" -Option Constant
+if (-not (Get-Variable -Name GITHUB_PROFILE_URL -ErrorAction SilentlyContinue)) {
+    $global:GITHUB_PROFILE_URL = "https://raw.githubusercontent.com/rendegosling/powershell-profile/main/Microsoft.Powershell_profile.ps1"
+}
 
 # Flag to prevent infinite loop
 if (-not (Get-Variable -Name ProfileUpdated -ErrorAction SilentlyContinue)) {
@@ -25,17 +27,16 @@ function Test-GitHubConnection {
 # Function to download and update the profile
 function Update-Profile {
     try {
-        Write-Host "Downloading profile from $GITHUB_PROFILE_URL" -ForegroundColor Yellow
-        $newProfile = Invoke-WebRequest -Uri $GITHUB_PROFILE_URL -UseBasicParsing | Select-Object -ExpandProperty Content
+        Write-Host "Downloading profile from $global:GITHUB_PROFILE_URL" -ForegroundColor Yellow
+        $newProfile = Invoke-WebRequest -Uri $global:GITHUB_PROFILE_URL -UseBasicParsing | Select-Object -ExpandProperty Content
         Write-Host "Profile content downloaded successfully." -ForegroundColor Yellow
         
-        Write-Host "Searching for version in new profile..." -ForegroundColor Yellow
+        Write-Host "Checking for new version..." -ForegroundColor Yellow
         $newVersionMatch = $newProfile -match '\$profileVersion\s*=\s*"([\d\.]+)"'
         if ($newVersionMatch) {
             $newVersion = $Matches[1]
-            Write-Host "New version found: $newVersion" -ForegroundColor Yellow
-            
             if ([System.Management.Automation.SemanticVersion]$newVersion -gt [System.Management.Automation.SemanticVersion]$profileVersion) {
+                Write-Host "New version found: $newVersion" -ForegroundColor Yellow
                 Set-Content -Path $PROFILE -Value $newProfile
                 Write-Host "Profile updated successfully to version $newVersion." -ForegroundColor Green
                 return $true
@@ -63,7 +64,8 @@ if ($global:GitHubConnected -and -not $global:ProfileUpdated) {
     $updated = Update-Profile
     if ($updated) {
         $global:ProfileUpdated = $true
-        Write-Host "Profile has been updated. Please restart PowerShell for changes to take effect." -ForegroundColor Yellow
+        Write-Host "Profile has been updated. Reloading..." -ForegroundColor Yellow
+        . $PROFILE
         return
     }
 }
